f8f37edfe7de82531dbecf31664b75a0942c18fc
Prieto <web25aprieto@MALTUNA.LOCAL>
2025-11-17 14:54:42 +0100

    Gazta klasea sortuta

---
+++ src/lacturale/main.java
@@ -0,0 +1,14 @@
+package lacturale;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gazta g =new Gazta();
+		
+		g.lortuPisua();
+		
+	}
+
+}
